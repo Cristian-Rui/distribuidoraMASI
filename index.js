8f18024c8b6d2c101743625e8ecb5a617ba0d17e
Cristian Rui <cristian.rui98@gmail.com>
2023-05-14 20:06:59 -0300

    avance del html y css

--- index.js
+++ index.js
@@ -12,43 +12,64 @@ while (edad < 18) {
 
 alert("bienvenido a la pagina de distribuidora masi");
 
-let compra = prompt("¿que desea comprar?: \nwhisky 1: $10000\nwhisky 2: $15000\nwhisky 3: $150000\nwhisky 4: $500000")
+// let compra = prompt("¿que desea comprar?: \nwhisky 1: $10000\nwhisky 2: $15000\nwhisky 3: $150000\nwhisky 4: $500000")
 
-let subtotal = 0
+// let subtotal = 0
 
-let carrito = (subtotal, x) => subtotal + x
+// let carrito = (subtotal, x) => subtotal + x
 
-const infoCarrito = (compra,subtotal) => alert(`Se añadio 1(uno) ${compra} a tu carrito\nEl subtotal de tu compra es de $${subtotal}`);
+// const infoCarrito = (compra,subtotal) => alert(`Se añadio 1(uno) ${compra} a tu carrito\nEl subtotal de tu compra es de $${subtotal}`);
 
-while (compra != "no") {
-    switch (compra) {
-        case "whisky 1":
-            subtotal = carrito(subtotal,10000);
-            infoCarrito(compra,subtotal);
-            break;
-        case "whisky 2":
-            subtotal = carrito(subtotal, 15000);
-            infoCarrito(compra,subtotal);
-            break;
-        case "whisky 3":
-            subtotal = carrito(subtotal, 150000);
-            infoCarrito(compra,subtotal);
-            break;
-        case "whisky 4":
-            subtotal = carrito(subtotal, 500000);
-            infoCarrito(compra,subtotal);
-            break;
-        default:
-            alert("En este momento no se encuentra disponible esa opcion");
-            break;
+// while (compra != "no") {
+//     switch (compra) {
+//         case "whisky 1":
+//             subtotal = carrito(subtotal,10000);
+//             infoCarrito(compra,subtotal);
+//             break;
+//         case "whisky 2":
+//             subtotal = carrito(subtotal, 15000);
+//             infoCarrito(compra,subtotal);
+//             break;
+//         case "whisky 3":
+//             subtotal = carrito(subtotal, 150000);
+//             infoCarrito(compra,subtotal);
+//             break;
+//         case "whisky 4":
+//             subtotal = carrito(subtotal, 500000);
+//             infoCarrito(compra,subtotal);
+//             break;
+//         default:
+//             alert("En este momento no se encuentra disponible esa opcion");
+//             break;
+//     }
+
+//     let seguirComprando = prompt("¿desea seguir comprando?")
+//     if (seguirComprando == "si"){
+//         compra = prompt("¿que desea comprar?: \nwhisky 1: $10000\nwhisky 2: $15000\nwhisky 3: $150000\nwhisky 4: $500000")
+//     }else{
+//         compra = "no"
+//     }
+
+// }
+
+class Producto {
+    constructor(obj) {
+        this.nombre = obj.nombre,
+        this.tipo = obj.tipo,
+        this.tamaño = parseInt(obj.tamaño),
+        this.precio = parceInt(obj.precio);
     }
-    
-    let seguirComprando = prompt("¿desea seguir comprando?")
-    if (seguirComprando == "si"){
-        compra = prompt("¿que desea comprar?: \nwhisky 1: $10000\nwhisky 2: $15000\nwhisky 3: $150000\nwhisky 4: $500000")
-    }else{
-        compra = "no"
+    hablar () {
+        console.log("hola mi nombre es " + this.nombre)
+    }
+        cambiarNombre = (nuevoPrecio) => this.precio = nuevoPrecio
     }
 
-}
-
+const BlueLabel750 = new Producto(
+    {
+        nombre: 'Johnnie Walker Blue Label',
+        tipo: 'whisky',
+        tamaño: 750,
+        precio: 91079
+    }
+)
