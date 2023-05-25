3363f843bb730c53252a0b3bded6f66d28754ee3
Valentina Rui <cristian.rui98@gmail.com>
2023-05-25 19:17:17 -0300

    modificacion del footer

---
+++ script.js
@@ -0,0 +1,53 @@
+// comentarios en una linea
+
+/* comentario
+de 
+multiples 
+lineas*/
+
+//js no toma en cuenta los espacios en blanco entre lineas
+
+//usamos let para definir una variable, por lo tanto su contenido puede variar
+/* let nombreUsuario = "Cristian";
+let apellidoUsuario = "Rui" */
+
+// a js le es indiferente si las lineas terminan con ; pero siempre es mejor usarlo
+
+//para definir constantes usamos const
+/* const anio = 2023; */
+//siempre lo mejor es no utilizar el caracter ñ
+
+/* let numero1 = 80
+let numero2 = 90
+
+let resultadoMultplicacion = numero1 * numero2 
+
+let resta = numero1 - numero2
+
+numero1 = 10000
+
+resultadoMultplicacion =  resultadoMultplicacion * numero1 */
+
+// para reasignar valores ya no usamos la palabra reservada let
+
+/* let datosUsuario = nombreUsuario +" "+ apellidoUsuario
+ */
+
+//usamos console para mostrar contendo por pantalla
+/* console.log("este es un msj en consola")
+console.log(23)
+console.log(datosUsuario) */
+
+
+//usamos alert para que nos muestre una alerta sin ingresar a la consola
+
+
+/* let nombreUsuario = prompt("Ingrese su nombre:")
+let apellidoUsuario = prompt("Ingrese su apellido:")
+
+let datosUsuario = `Hola ${nombreUsuario} ${apellidoUsuario}, como estas?`
+
+alert(datosUsuario) */
+
+//ciclos bucles e iteraciones
+
